94601c60377dcaf0071c785aaa8ae2a468d15c58
Ludvig Malm <malmludvig@gmail.com>
2026-01-29 15:24:48 +0100

    Added all files

---
+++ WebAPIWithCrud/Controllers/ItemsController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using WebAPIWithCrud.Models;
+using WebAPIWithCrud.Services;
+
+namespace WebAPIWithCrud.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ItemsController : ControllerBase
+{
+    private readonly ItemsService _itemsService;
+
+    public ItemsController(ItemsService itemsService)
+    {
+        _itemsService = itemsService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Items>> GetAll()
+    {
+        return Ok(_itemsService.GetAll());
+    }
+
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Items> Get(int id)
+    {
+        var item = _itemsService.GetById(id);
+        if (item == null)
+            return NotFound();
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public ActionResult<Items> Create(Items item)
+    {
+        var created = _itemsService.Create(item);
+        return Created($"/api/items/{created.Id}", created);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Update(int id, Items item)
+    {
+        if (!_itemsService.Update(id, item))
+            return NotFound();
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        if (!_itemsService.Delete(id))
+            return NotFound();
+        return NoContent();
+    }
+}
---
+++ WebAPIWithCrud/Models/Items.cs
@@ -0,0 +1,12 @@
+namespace WebAPIWithCrud.Models;
+
+
+//Model
+public class Items
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Password { get; set; } = "";
+}
+
+
